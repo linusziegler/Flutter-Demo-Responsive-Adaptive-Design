5f87fcb07e8c218028388005e3f69790950fdf46
Linus Ziegler <linus.ziegler@outlook.de>
2024-04-25 14:09:19 +0200

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_adaptive_demo/responsive_layouts.dart';
 import 'basic_widgets.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+
+bool get isMobileDevice => !kIsWeb && (Platform.isIOS || Platform.isAndroid);
+bool get isDesktopDevice => !kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux);
+bool get isMobileDeviceOrWeb => kIsWeb || isMobileDevice;
+bool get isDesktopDeviceOrWeb => kIsWeb || isDesktopDevice;
 
 void main() {
   runApp(const MainApp());
@@ -15,7 +22,7 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
             appBar: AppBar(
-              title: const Text('Responsive Widgets Demo'),
+              title: const Text('Responsive / Adaptive Widgets Demo'),
             ),
             body: ListView(
               children: const [
@@ -33,6 +40,16 @@ class MainApp extends StatelessWidget {
                 Row(children: [
                   SizedBox(width: 10),
                   ExpandingWidget(),
+                  SizedBox(width: 10),
+                  ExpandingWidget(),
+                  SizedBox(width: 10),
+                  ExpandingWidget(),
+                  SizedBox(width: 10),
+                ]),
+                BasicTitle(content: "Combine Fixed & Expanding Widgets"),
+                Row(children: [
+                  SizedBox(width: 10),
+                  BasicWidget(),
                   SizedBox(width: 10),
                   ExpandingWidget(),
                   SizedBox(width: 10),
@@ -62,12 +79,25 @@ class MainApp extends StatelessWidget {
                 ]),
                 BasicTitle(content: "Responsive Scaling using Media Query"),
                 ResponsiveShape(),
-                BasicTitle(content: "Dynamic Layout using Layoutbuilder"),
+                BasicTitle(content: "Adaptive Layout using Layoutbuilder"),
                 ResponsiveLayout(
                       mobileLayout: MobileLayout(),
                       tabletLayout: TabletLayout(),
                       desktopLayout: DesktopLayout()
                       ),
+                BasicTitle(content: "Adaptive Widgets"),
+                Column(
+                  children: [
+                  SizedBox(height: 10),
+                  Switch.adaptive(value: true, onChanged: null),
+                  SizedBox(height: 10),
+                  Slider.adaptive(value: 0.5, onChanged: null),
+                  SizedBox(height: 10),
+                  Checkbox.adaptive(value: true, onChanged: null),
+                  SizedBox(height: 10),
+                  Radio.adaptive(value: false, groupValue: false, onChanged: null),
+                  SizedBox(height: 10),
+                ]),          
               ],
             )));
   }
